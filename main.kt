9b17eee4b486aa7c22a08fc22444a8b293c0499b
POPOGOS <83871451+POPOGOS@users.noreply.github.com>
2024-10-10 12:34:32 +0800

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,98 @@
+
+
+enum class Direction{
+    UP,LEFT,DOWN,RIGHT
+}
+
+open class Robot(var x:Int,var y:Int,var direction:Direction){
+    var getx: Int = 0
+        get() {
+            return x
+        }
+
+    var gety: Int = 0
+        get() {
+            return y
+        }
+
+    var getDirection: Direction = Direction.UP
+        get() {
+            return direction
+        }
+
+    fun TurnLeft():Direction {
+        direction = when(direction){
+            Direction.UP -> Direction.LEFT
+            Direction.LEFT -> Direction.DOWN
+            Direction.DOWN -> Direction.RIGHT
+            Direction.RIGHT -> Direction.UP
+        }
+        return direction
+    }
+    fun TurnRight():Direction{
+        direction = when(direction){
+            Direction.UP -> Direction.RIGHT
+            Direction.RIGHT -> Direction.DOWN
+            Direction.DOWN -> Direction.LEFT
+            Direction.LEFT -> Direction.UP
+        }
+        return direction
+    }
+    fun StepForvard():Int{
+        when(direction){
+            Direction.UP -> y++
+            Direction.LEFT -> x--
+            Direction.DOWN -> y--
+            Direction.RIGHT -> x++
+        }
+        when(direction){
+            Direction.UP -> return y
+            Direction.DOWN -> return y
+            Direction.RIGHT -> return x
+            Direction.LEFT-> return x
+
+        }
+    }
+}
+class SmartRobot(var robot:Robot,var toX:Int,var toY: Int){
+    fun GoToPoint(){
+        if(robot.getx < toX){
+            while(robot.getDirection != Direction.LEFT){
+                robot.TurnLeft()
+            }
+            while(robot.getx != toX){
+                robot.x --
+            }
+        }else{
+            while(robot.getDirection != Direction.RIGHT){
+                robot.TurnRight()
+            }
+            while(robot.getx != toX){
+                robot.x ++
+            }
+        }
+        if(robot.gety < toY){
+            while(robot.getDirection != Direction.UP){
+                robot.TurnLeft()
+            }
+            while(robot.gety!= toY){
+                robot.y ++
+            }
+        }else{
+            while(robot.getDirection != Direction.DOWN){
+                robot.TurnRight()
+            }
+            while(robot.gety!= toY){
+                robot.y --
+            }
+        }
+    }
+}
+fun main(){
+    val robot = Robot(0, 0, Direction.UP)
+    println("${robot.getx},${robot.gety}")
+    val smartRobot = SmartRobot(robot,15,3)
+    smartRobot.GoToPoint()
+    println("${robot.getx},${robot.gety}")
+}
+
